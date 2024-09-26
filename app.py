b0715975ae469818b6793485ebb872ccf7dadc32
Shad Spreiter <loyaldogtech@gmail.com>
2024-09-26 09:24:07 -0700

    Task 3 completed

--- app.py
+++ app.py
@@ -13,3 +13,17 @@ if authorized_user == "":
     print("You must be logged in to donate")
 else:
     print(f"Logged in as {authorized_user}")
+
+while True:
+    choice = input("What would you like to do? ").lower()
+    if choice == "1":
+        print("TODO: Write Login Functionality")
+    elif choice == "2":
+        print("TODO: Write Register Functionality")
+    elif choice == "3":
+        print("TODO: Write Donate Functionality")
+    elif choice == "4":
+        print("TODO: Write Show Donations Functionality")
+    elif choice == "5":
+        print("Bye!")
+        break
