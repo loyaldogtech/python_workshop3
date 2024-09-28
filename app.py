f303df03b997f107d0903e60c06b8202f8c88d26
Shad Spreiter <loyaldogtech@gmail.com>
2024-09-27 21:02:00 -0700

    Task 6 completed

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from donations_pkg.user import login, register
-from donations_pkg.homepage import show_homepage
-
+from donations_pkg.homepage import show_homepage, donate
+ 
 database = {
     "admin": "password123",
 }
@@ -28,7 +28,11 @@ while True:
         if authorized_user != "":
             database[username] = password
     elif choice == "3":
-        print("TODO: Write Donate Functionality")
+        if authorized_user == "":
+            print("You are not logged in.")
+        else:
+            donation_string = donate(authorized_user)
+            donations.append(donation_string)
     elif choice == "4":
         print("TODO: Write Show Donations Functionality")
     elif choice == "5":
--- donations_pkg/homepage.py
+++ donations_pkg/homepage.py
@@ -9,3 +9,8 @@ def show_homepage():
     print("|        5.   Exit                      |")
     print("-----------------------------------------")
 
+def donate(username):
+    donation_amt = float(input("Enter amount to donate: "))
+    donation_string = f"{username} donated ${donation_amt}"
+    print("Thank you for your donation!")
+    return donation_string
